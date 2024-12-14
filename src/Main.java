8bf39ecf52fa29a0c02fcb276d4dde85f83c4b73
artem <artem.dos.25@gmail.com>
2024-12-14 19:01:49 +0200

    Added ExampleStringUsage methods and realization

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        ExampleStringUsage.findSymbolOccurance("Hello", 'l');
 
+        int checker = ExampleStringUsage.findWordPosition("Apollo", "pollo");
+        System.out.println(checker);
+        checker = ExampleStringUsage.findWordPosition("Apple", "Plant");
+        System.out.println(checker);
+
+        ExampleStringUsage.stringReverse("Hello");
+
+        ExampleStringUsage.isPalindrome("ERE");
+        ExampleStringUsage.isPalindrome("Allo");
+
+        ExampleStringUsage.stringGame();
     }
 }
